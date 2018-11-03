8a13405017f488dcec74cb84a3f44d6bc21d771a
sytraaaaajackeom <44702571+sytraaaaajackeom@users.noreply.github.com>
2018-11-03 19:36:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('ready', () => {
 });
 
 client.on("message", message => {
-    var prefix = "! ";
+    var prefix = "!";
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
